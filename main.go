a948cbfbebeeb00bc7fbab7580e41ba35369f127
Ahmed Hassan <ahmed.sayed.hassan@ibm.com>
2020-05-18 16:34:34 +0800

    Add sample for main.go

--- main.go
+++ main.go
@@ -3,7 +3,15 @@ package main
 import "github.com/CHasCode/ceas"
 
 func main() {
-	//ceas.CreateClusterConfig(1, 3)
+	// Create Cluster 1 master nodes and 3 workers
+	// Install Nignx Ingress on the control-plan
 	ceas.CreateCluster(1, 3)
-
+	// Build a docker from source code and load it to the cluster registry not required
+	ceas.BuildAndLoadDocker("C:\\Users\\AHMEDSAYEDHASSANABDE\\nodejs-demo", "nodejsdemo:latest")
+	// Deploy app on the cluster
+	ceas.DeployApp("C:\\Users\\AHMEDSAYEDHASSANABDE\\nodejs-demo\\deployment.yaml")
+	// Remove app from the cluster
+	ceas.RemoveApp("C:\\Users\\AHMEDSAYEDHASSANABDE\\nodejs-demo\\deployment.yaml")
+	// Destroy the kubernetes cluster
+	ceas.DeleteCluster()
 }
